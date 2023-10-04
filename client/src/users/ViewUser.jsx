047c571dee05dc4a5f20e9662386cc4d58e41181
Muhammad Ramadhan <131851453+autumnleaf-ra@users.noreply.github.com>
2023-10-04 14:23:45 +0700

    Update ViewUser.jsx

--- client/src/users/ViewUser.jsx
+++ client/src/users/ViewUser.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
 import { API_URL } from "../config";
+import { useParams, Link } from "react-router-dom";
 
 export default function ViewUser() {
   let { id } = useParams();
